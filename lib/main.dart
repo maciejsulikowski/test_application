b0ab28a2986dadd46cfe953fd2e304fa8cc4ae25
macieksulikowski <suleymaniana99@gmail.com>
2024-09-23 20:52:38 +0200

    Change name to check if code can be pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
